b0771ab2ea73f9ef66f81c544c860c45317c31b5
CatalinCaldararu <catalin.caldararu99@gmail.com>
2022-03-16 21:19:27 +0200

    Add appointments and prescriptions repositories

--- BlazorCRUD/Data/Classes/Appointment.cs
+++ BlazorCRUD/Data/Classes/Appointment.cs
@@ -12,6 +12,6 @@ namespace BlazorCRUD.Data
         public int AppointmentId { get; set; }
         public int Medic { get; set; }
         public int Patient { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime Date { get; set; } = DateTime.UtcNow;
     }
 }
