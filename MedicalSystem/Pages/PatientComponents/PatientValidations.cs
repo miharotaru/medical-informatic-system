8ebf074905f34aa79f36f2ff7dda688030cfe7fc
CatalinCaldararu <catalin.caldararu99@gmail.com>
2022-05-28 11:30:03 +0300

    Add functions to prepare for the patient field validations

---
+++ MedicalSystem/Pages/PatientComponents/PatientValidations.cs
@@ -0,0 +1,44 @@
+﻿using MedicalSystems.Data;
+
+namespace MedicalSystem.Pages.PatientComponents
+{
+    public class PatientValidations
+    {
+        public static bool ValidatePatient(string name, string email)
+        {
+            if (name.Length > 0 && email.Length > 0 && StartsWithUpperCase(name) && EmailContainsAtCharacter(email) && EmailEndsWithDotCom(email))
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        private static bool EmailContainsAtCharacter (string email)
+        {
+            if (email.Contains("@"))
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        private static bool EmailEndsWithDotCom(string email)
+        {
+            if (email.EndsWith(".com"))
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        private static bool StartsWithUpperCase(string name)
+        {
+            if (char.IsUpper(name[0]))
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+
+}
