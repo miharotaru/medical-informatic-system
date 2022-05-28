9aeac7e6d10f7bce1850de993c249b7e13254781
miharotaru <mihaelarot3@gmail.com>
2022-05-28 13:11:48 +0300

    commit

--- MedicalSystem/obj/Debug/netcoreapp5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ MedicalSystem/obj/Debug/netcoreapp5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,63 +13,63 @@ namespace MedicalSystem.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Catalin\Desktop\Medical System\medical-informatic-system\MedicalSystem\_Imports.razor"
+#line 1 "C:\Users\Mihaela\Desktop\licenta_aplicatie\medical-informatic-system\MedicalSystem\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Catalin\Desktop\Medical System\medical-informatic-system\MedicalSystem\_Imports.razor"
+#line 2 "C:\Users\Mihaela\Desktop\licenta_aplicatie\medical-informatic-system\MedicalSystem\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Catalin\Desktop\Medical System\medical-informatic-system\MedicalSystem\_Imports.razor"
+#line 3 "C:\Users\Mihaela\Desktop\licenta_aplicatie\medical-informatic-system\MedicalSystem\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Catalin\Desktop\Medical System\medical-informatic-system\MedicalSystem\_Imports.razor"
+#line 4 "C:\Users\Mihaela\Desktop\licenta_aplicatie\medical-informatic-system\MedicalSystem\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Catalin\Desktop\Medical System\medical-informatic-system\MedicalSystem\_Imports.razor"
+#line 5 "C:\Users\Mihaela\Desktop\licenta_aplicatie\medical-informatic-system\MedicalSystem\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Catalin\Desktop\Medical System\medical-informatic-system\MedicalSystem\_Imports.razor"
+#line 6 "C:\Users\Mihaela\Desktop\licenta_aplicatie\medical-informatic-system\MedicalSystem\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Catalin\Desktop\Medical System\medical-informatic-system\MedicalSystem\_Imports.razor"
+#line 7 "C:\Users\Mihaela\Desktop\licenta_aplicatie\medical-informatic-system\MedicalSystem\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Catalin\Desktop\Medical System\medical-informatic-system\MedicalSystem\_Imports.razor"
+#line 8 "C:\Users\Mihaela\Desktop\licenta_aplicatie\medical-informatic-system\MedicalSystem\_Imports.razor"
 using MedicalSystems;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Catalin\Desktop\Medical System\medical-informatic-system\MedicalSystem\_Imports.razor"
+#line 9 "C:\Users\Mihaela\Desktop\licenta_aplicatie\medical-informatic-system\MedicalSystem\_Imports.razor"
 using MedicalSystem.Shared;
 
 #line default
@@ -83,7 +83,7 @@ using MedicalSystem.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 32 "C:\Users\Catalin\Desktop\Medical System\medical-informatic-system\MedicalSystem\Shared\NavMenu.razor"
+#line 32 "C:\Users\Mihaela\Desktop\licenta_aplicatie\medical-informatic-system\MedicalSystem\Shared\NavMenu.razor"
        
     bool collapseNavMenu = true;
 
