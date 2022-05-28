5e0b9d1c5f34d3c0595fd8dc16ea061eee856af3
CatalinCaldararu <catalin.caldararu99@gmail.com>
2022-05-28 13:18:27 +0300

    commit

--- MedicalSystem/obj/Debug/netcoreapp5.0/RazorDeclaration/Pages/MedicComponents/UpdateMedic.razor.g.cs
+++ MedicalSystem/obj/Debug/netcoreapp5.0/RazorDeclaration/Pages/MedicComponents/UpdateMedic.razor.g.cs
@@ -13,70 +13,70 @@ namespace MedicalSystem.Pages.MedicComponents
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Mihaela\Desktop\licenta_aplicatie\medical-informatic-system\MedicalSystem\_Imports.razor"
+#line 1 "C:\Users\Catalin\Desktop\Medical System\medical-informatic-system\MedicalSystem\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Mihaela\Desktop\licenta_aplicatie\medical-informatic-system\MedicalSystem\_Imports.razor"
+#line 2 "C:\Users\Catalin\Desktop\Medical System\medical-informatic-system\MedicalSystem\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Mihaela\Desktop\licenta_aplicatie\medical-informatic-system\MedicalSystem\_Imports.razor"
+#line 3 "C:\Users\Catalin\Desktop\Medical System\medical-informatic-system\MedicalSystem\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Mihaela\Desktop\licenta_aplicatie\medical-informatic-system\MedicalSystem\_Imports.razor"
+#line 4 "C:\Users\Catalin\Desktop\Medical System\medical-informatic-system\MedicalSystem\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Mihaela\Desktop\licenta_aplicatie\medical-informatic-system\MedicalSystem\_Imports.razor"
+#line 5 "C:\Users\Catalin\Desktop\Medical System\medical-informatic-system\MedicalSystem\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Mihaela\Desktop\licenta_aplicatie\medical-informatic-system\MedicalSystem\_Imports.razor"
+#line 6 "C:\Users\Catalin\Desktop\Medical System\medical-informatic-system\MedicalSystem\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Mihaela\Desktop\licenta_aplicatie\medical-informatic-system\MedicalSystem\_Imports.razor"
+#line 7 "C:\Users\Catalin\Desktop\Medical System\medical-informatic-system\MedicalSystem\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Mihaela\Desktop\licenta_aplicatie\medical-informatic-system\MedicalSystem\_Imports.razor"
+#line 8 "C:\Users\Catalin\Desktop\Medical System\medical-informatic-system\MedicalSystem\_Imports.razor"
 using MedicalSystems;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Mihaela\Desktop\licenta_aplicatie\medical-informatic-system\MedicalSystem\_Imports.razor"
+#line 9 "C:\Users\Catalin\Desktop\Medical System\medical-informatic-system\MedicalSystem\_Imports.razor"
 using MedicalSystem.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Mihaela\Desktop\licenta_aplicatie\medical-informatic-system\MedicalSystem\Pages\MedicComponents\UpdateMedic.razor"
+#line 2 "C:\Users\Catalin\Desktop\Medical System\medical-informatic-system\MedicalSystem\Pages\MedicComponents\UpdateMedic.razor"
 using MedicalSystems.Data;
 
 #line default
@@ -91,7 +91,7 @@ using MedicalSystems.Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 40 "C:\Users\Mihaela\Desktop\licenta_aplicatie\medical-informatic-system\MedicalSystem\Pages\MedicComponents\UpdateMedic.razor"
+#line 40 "C:\Users\Catalin\Desktop\Medical System\medical-informatic-system\MedicalSystem\Pages\MedicComponents\UpdateMedic.razor"
         [Parameter]
             public string CurrentID { get; set; }
             Medic objMedic = new Medic();
